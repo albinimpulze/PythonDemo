140125bf52e92d53172493754ab149a21c964b05
albinimpulze <mailalbinxavier@gmail.com>
2024-03-29 16:19:53 +0530

    first commit

---
+++ listcom.py
@@ -0,0 +1,4 @@
+num = [i for i in range(1,11) if i % 2 ==0]
+
+
+print(num)
---
+++ set.py
@@ -0,0 +1,4 @@
+rj = {"name":"Rajkumar", "name" : "Priyanshu" ,"mobile":9744526252, "Address":"Ujjain" , "Email":"rj@gmail.com"}
+
+for i in rj.items(-):
+    print(i)
